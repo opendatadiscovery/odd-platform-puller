637a73237ebdae589f664f65959f5191841df769
Nikita Dementev <ndementev@provectus.com>
2021-12-14 13:01:54 +0300

    Add MaxInMemorySize puller client setting

--- src/main/java/com/provectus/oddplatform/puller/config/PullerConfiguration.java
+++ src/main/java/com/provectus/oddplatform/puller/config/PullerConfiguration.java
@@ -5,7 +5,6 @@ import com.provectus.oddplatform.puller.dto.DataSourceDto;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
@@ -22,19 +22,45 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableScheduling
 @EnableConfigurationProperties(OAuthProperties.class)
 public class PullerConfiguration {
     @Bean
-    public WebClient platformWebClient(@Value("${platform.host-url}") final String platformHost) {
+    public WebClient platformWebClient(
+        @Value("${platform.host-url}") final String platformHost,
+        @Value("${puller.client.max-in-memory-size}") final Integer maxInMemorySize
+    ) {
         return WebClient.builder()
             .baseUrl(platformHost)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .exchangeStrategies(ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(maxInMemorySize))
+                .build())
             .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000)
-                .responseTimeout(Duration.ofMillis(30000))))
+                .responseTimeout(Duration.ofMillis(30000))
+                .doOnConnected(conn ->
+                    conn.addHandlerLast(new ReadTimeoutHandler(30000, TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(30000, TimeUnit.MILLISECONDS)))))
+            .build();
+    }
+
+    @Bean
+    public WebClient adapterWebClient(@Value("${puller.client.max-in-memory-size}") final Integer maxInMemorySize) {
+        return WebClient.builder()
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .exchangeStrategies(ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(maxInMemorySize))
+                .build())
+            .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
+                .responseTimeout(Duration.ofMillis(30000))
+                .doOnConnected(conn ->
+                    conn.addHandlerLast(new ReadTimeoutHandler(30000, TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(30000, TimeUnit.MILLISECONDS)))))
             .build();
     }
 
--- src/main/java/com/provectus/oddplatform/puller/service/AdapterRestServiceImpl.java
+++ src/main/java/com/provectus/oddplatform/puller/service/AdapterRestServiceImpl.java
@@ -1,37 +1,24 @@
 package com.provectus.oddplatform.puller.service;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.opendatadiscovery.client.model.DataEntityList;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 import java.net.URI;
 import java.net.URL;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 public class AdapterRestServiceImpl implements AdapterRestService {
-    private final WebClient webClient = WebClient.builder()
-        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-        .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
-            .responseTimeout(Duration.ofMillis(30000))
-            .doOnConnected(conn ->
-                conn.addHandlerLast(new ReadTimeoutHandler(30000, TimeUnit.MILLISECONDS))
-                    .addHandlerLast(new WriteTimeoutHandler(30000, TimeUnit.MILLISECONDS)))))
-        .build();
+    private final WebClient webClient;
 
+    public AdapterRestServiceImpl(@Qualifier("adapterWebClient") final WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @Override
     public Mono<DataEntityList> getEntities(final URL adapterHost) {
